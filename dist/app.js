e2ac8595bbcf2ba89c992833af6de47641e30041
suman <ronaldosuman97@gmail.com>
2022-06-04 10:34:35 +0530

    initial commit

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+const newElement = document.createElement("div");
+newElement.innerText = "Hello";
+newElement.style.color = "blue";
+document.body.append(newElement);
---
+++ src/app.ts
@@ -0,0 +1,4 @@
+const newElement = document.createElement("div");
+newElement.innerText = "Hello";
+newElement.style.color = "blue";
+document.body.append(newElement);
