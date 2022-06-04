4c479b2ed29d985b606976072613aa7ca49a33ad
suman <ronaldosuman97@gmail.com>
2022-06-04 10:57:10 +0530

    methods added

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,24 @@
-const newElement = document.createElement("div");
-newElement.innerText = "Hello";
-newElement.style.color = "blue";
-document.body.append(newElement);
+// Adding Element
+document.body.append("Hello");
+
+// Creating Element
+const divElement = document.createElement("div");
+divElement.innerText = "Hello Again";
+document.body.append(divElement);
+document.querySelector("div")!.style.backgroundColor = "green";
+
+// Inserting Element in the div
+const h1Element = document.createElement("h1");
+h1Element.innerText = "h1";
+h1Element.style.backgroundColor = "yellow";
+document.querySelector("div")!.append(h1Element);
+
+// Modifying Attributes
+divElement.setAttribute("id", "main_container");
+h1Element.className = "header_sub";
+h1Element.classList.add("header");
+
+// Removing Elements
+//* h1Element.remove();
+const h1Class = h1Element.getAttribute("class");
+console.log(h1Class);
